9052aaaed63478e36366758f4f9522b608e2be7a
mpcarino <mps.carino101@gmail.com>
2022-10-07 11:38:02 +0800

    Setup project

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Marwin Carino on 10/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
