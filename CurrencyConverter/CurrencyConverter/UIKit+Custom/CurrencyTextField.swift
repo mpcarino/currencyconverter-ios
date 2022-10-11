234029cdc1727709700cbb95f25c974830389e26
mpcarino <mps.carino101@gmail.com>
2022-10-11 22:14:45 +0800

    Add haptic feedback when editing currency textfield

--- CurrencyConverter/CurrencyConverter/UIKit+Custom/CurrencyTextField.swift
+++ CurrencyConverter/CurrencyConverter/UIKit+Custom/CurrencyTextField.swift
@@ -7,49 +7,35 @@
 
 import UIKit
 
-import RxSwift
-import RxCocoa
 import NSObject_Rx
+import RxCocoa
+import RxSwift
 
 class CurrencyTextField: UITextField {
+  // MARK: - Properties
+
+  private let generator = UIImpactFeedbackGenerator(style: .light)
+
   var amount: Decimal = .zero
+
   var currency: Currency = .default {
     didSet {
       updatePlaceHolder()
-      
     }
   }
-  
+
   override init(frame: CGRect) {
     super.init(frame: frame)
     prepare()
   }
-  
+
   required init?(coder: NSCoder) {
     super.init(coder: coder)
   }
-  
+
   override func awakeFromNib() {
     super.awakeFromNib()
     prepare()
-  }
-  
-  private func prepare() {
-    configure()
-    bind()
-  }
-  
-  private func configure() {
-    autocapitalizationType = .none
-    autocorrectionType = .no
-    spellCheckingType = .no
-    keyboardType = .decimalPad
-    returnKeyType = .done
-    enablesReturnKeyAutomatically = true
-  }
-  
-  private func updatePlaceHolder() {
-    placeholder = currency.currencyFormatter.string(amount: NSNumber(value: 0.0))
   }
 }
 
@@ -58,15 +44,21 @@ class CurrencyTextField: UITextField {
 private extension CurrencyTextField {
   private func bind() {
     rx.controlEvent(.editingDidBegin)
-      .subscribe(onNext: { [unowned self]  in
+      .subscribe(onNext: { [unowned self] in
         self.text = nil
         self.amount = .zero
       })
       .disposed(by: rx.disposeBag)
-    
+
     rx.controlEvent(.editingDidEnd)
-      .subscribe(onNext: { [unowned self]  in
+      .subscribe(onNext: { [unowned self] in
         self.formatText()
+      })
+      .disposed(by: rx.disposeBag)
+
+    rx.controlEvent(.editingChanged)
+      .subscribe(onNext: { [unowned self] in
+        generator.impactOccurred()
       })
       .disposed(by: rx.disposeBag)
   }
@@ -76,10 +68,32 @@ private extension CurrencyTextField {
 
 extension CurrencyTextField {
   func formatText() {
-    if let text = self.text,
+    if let text = text,
        let decimal = Decimal(string: text) {
-      self.amount = decimal
-      self.text = self.currency.currencyFormatter.string(amount: self.amount as NSNumber)
+      amount = decimal
+      self.text = currency.currencyFormatter.string(amount: amount as NSNumber)
     }
+  }
+}
+
+// MARK: - Helpers
+
+private extension CurrencyTextField {
+  func prepare() {
+    configure()
+    bind()
+  }
+
+  func configure() {
+    autocapitalizationType = .none
+    autocorrectionType = .no
+    spellCheckingType = .no
+    keyboardType = .decimalPad
+    returnKeyType = .done
+    enablesReturnKeyAutomatically = true
+  }
+
+  func updatePlaceHolder() {
+    placeholder = currency.currencyFormatter.string(amount: NSNumber(value: 0.0))
   }
 }
